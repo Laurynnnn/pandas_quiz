a7d338a692903ebf048e4fa7d41152a87d5416eb
Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>
2022-11-02 00:15:21 +0300

    Create number_4.py

---
+++ number_4.py
@@ -0,0 +1,9 @@
+#question 4 Create a dataframe with 10 rows and 3 columns, where the values are random numbers between 1 and 10 (inclusive). 
+#Then, replace all values greater than 5 with the value 10.
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randint(0,11,size=(10, 3)), columns=list('123'))
+df = df.replace(['6', '7', '8', '9'], '10')
+print(df)
