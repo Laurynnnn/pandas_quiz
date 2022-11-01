e1b65278118aa44f6d9864b5f685d08dfa4683b4
Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>
2022-11-01 23:07:08 +0300

    Create number_3.py

---
+++ number_3.py
@@ -0,0 +1,12 @@
+#question 3
+# What method can be used to get the number of non-NA values in a pandas dataframe?
+
+# A. df.count()
+
+# B. df.na_count()
+
+# C. df.na_values()
+
+# D. df.na_count()
+
+print("Answer is  A")
